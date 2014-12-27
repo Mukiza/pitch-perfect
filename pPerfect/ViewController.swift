a8e2cd359a8bdbee3736a70e7659710019d2979b
Andrew Mukiza <andrewmukiza@gmail.com>
2014-12-27 10:08:38 +0300

    add nav controller for window two

--- pPerfect/ViewController.swift
+++ pPerfect/ViewController.swift
@@ -10,6 +10,14 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var recordinLabel: UILabel!
+    @IBOutlet weak var stopBtn: UIButton!
+    
+    @IBAction func RecordAudio(sender: UIButton) {
+        recordinLabel.hidden = false
+        stopBtn.hidden = false
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
